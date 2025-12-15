82f0eda0bd33c06642f7f4714231b9710fef98fb
Tessa Gerasimova <tessagerasimova@gmail.com>
2025-12-15 21:39:22 +0200

    low view-class (views urls)

--- cats/views.py
+++ cats/views.py
@@ -1,21 +1,45 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
+# через view-класс:
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Cat
 from .serializers import CatSerializer
 
+class APICat(APIView):
+    def get(self, request):
+        cats = Cat.objects.all()
+        serializer = CatSerializer(cats, many=True)
+        return Response(serializer.data)
 
-@api_view(['GET', 'POST'])  # Разрешены только POST- и GET-запросы
-def cat_list(request):
-    # В случае POST-запроса добавим список записей в БД
-    if request.method == 'POST':
-        serializer = CatSerializer(data=request.data, many=True)
+    def post(self, request):
+        serializer = CatSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED) 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    # В случае GET-запроса возвращаем список всех котиков
-    cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
-    return Response(serializer.data) 
+
+
+
+# через view-функции:
+# from rest_framework import status
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+
+# from .models import Cat
+# from .serializers import CatSerializer
+
+
+# @api_view(['GET', 'POST'])  # Разрешены только POST- и GET-запросы
+# def cat_list(request):
+#     # В случае POST-запроса добавим список записей в БД
+#     if request.method == 'POST':
+#         serializer = CatSerializer(data=request.data, many=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     # В случае GET-запроса возвращаем список всех котиков
+#     cats = Cat.objects.all()
+#     serializer = CatSerializer(cats, many=True)
+#     return Response(serializer.data) 
