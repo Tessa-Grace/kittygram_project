d20cf684caa6fdfda712e83f148579b166c4c27a
Tessa Gerasimova <tessagerasimova@gmail.com>
2025-12-15 22:35:36 +0200

    view-classes (low and generic)

--- cats/views.py
+++ cats/views.py
@@ -1,23 +1,41 @@
-# через view-класс:
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+# через generic view-класс:
+from rest_framework import generics
 
 from .models import Cat
 from .serializers import CatSerializer
 
-class APICat(APIView):
-    def get(self, request):
-        cats = Cat.objects.all()
-        serializer = CatSerializer(cats, many=True)
-        return Response(serializer.data)
 
-    def post(self, request):
-        serializer = CatSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED) 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class CatList(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+
+# через view-класс:
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+
+# from .models import Cat
+# from .serializers import CatSerializer
+
+# class APICat(APIView):
+#     def get(self, request):
+#         cats = Cat.objects.all()
+#         serializer = CatSerializer(cats, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = CatSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED) 
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,12 +1,24 @@
-# через view-класс:
-# urls.py
+# через generic view-класс:
 from django.urls import path
 
-from cats.views import APICat
+from cats.views import CatList, CatDetail
 
 urlpatterns = [
-    path('cats/', APICat.as_view()),
+    path('cats/', CatList.as_view()),
+    path('cats/<int:pk>/', CatDetail.as_view()),
 ]
+
+
+
+# через view-класс:
+# urls.py
+# from django.urls import path
+
+# from cats.views import APICat
+
+# urlpatterns = [
+#     path('cats/', APICat.as_view()),
+# ]
 
 
 
